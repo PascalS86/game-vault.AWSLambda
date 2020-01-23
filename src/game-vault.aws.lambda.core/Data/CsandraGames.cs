f19531e4b4ff0e82960bb7c999d2f3bef93bace6
Pascal S <pascal.senjic@gmail.com>
2020-01-23 22:33:41 +0100

    Generell preparation for games backend

---
+++ src/game-vault.aws.lambda.core/Data/CsandraGames.cs
@@ -0,0 +1,10 @@
+namespace aws.lambda.core.Data{
+    
+    using Amazon.DynamoDBv2;
+    using Amazon.DynamoDBv2.DataModel;
+    using Amazon.DynamoDBv2.DocumentModel;
+
+    public class CsandraGame{
+
+    }
+}
---
+++ src/game-vault.aws.lambda.core/Data/DataClass.cs
@@ -0,0 +1,9 @@
+namespace aws.lambda.core.Data{
+    public class DataClass{
+        public string Client{get;set;}
+        public CsandraGame Data{get;set;}
+    }
+    public class ClientClass{
+        public string Client{get;set;}
+    }
+}
